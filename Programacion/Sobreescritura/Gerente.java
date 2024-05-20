ef9fe3099371657bd86f2432111fccbd717ea485
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-05-20 09:03:51 -0500

    feat: overriding / intro polimorfismo
    - se agrega tema de overriding
    - se agrega codigo de polimorfismo

---
+++ Programacion/Sobreescritura/Gerente.java
@@ -0,0 +1,16 @@
+package Programacion.Sobreescritura;
+
+public class Gerente extends Empleado {
+    
+    private String departamento;
+
+    public Gerente(String nombre, double sueldo, String departamento){
+        super(nombre, sueldo); // Llamada al constructor con argumentos de la clase padre
+        this.departamento = departamento;
+    }
+
+    @Override // Le inidica al compilador que estamos sobreescribiendo 
+    public String obtenerDetalles(){
+        return super.obtenerDetalles() + ", departamento: " + this.departamento; // se accede al comportamiento del metodo en la clase padre
+    }
+}
